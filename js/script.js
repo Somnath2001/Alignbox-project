6d57148ee80e9d392a8952ec9b4f65773716fd32
Somnath Dudhat <somnathdudhat222@gmail.com>
2021-06-28 20:34:09 +0530

    finally done

---
+++ js/script.js
@@ -0,0 +1,11 @@
+
+    
+    const mainNav = document.getElementById('js-menu');
+    const navBarToggle = document.getElementById('js-navbar-toggle');
+    
+    navBarToggle.addEventListener('click', function () {
+      mainNav.classList.toggle('active');
+    });
+    
+  
+   
